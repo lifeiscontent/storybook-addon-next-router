03fd65a0997a1144010a242109bc1c9f4c095815
Armand Abric <armand@forgebinaire.net>
2021-07-09 11:26:55 +0200

    Compatibility with next v11.2

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { action } from '@storybook/addon-actions';
 import { makeDecorator } from '@storybook/addons';
 import React from 'react';
 import Router, { NextRouter } from 'next/router';
-import { RouterContext } from 'next/dist/next-server/lib/router-context';
+import { RouterContext } from 'next/dist/shared/lib/router-context';
 
 export const withNextRouter = makeDecorator({
   name: 'NextRouter',
